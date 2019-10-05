89431cd4ac2515890c090295d63ccea3bdc5dc93
SAMYUKTHA <38600655+samyukthagopalsamy@users.noreply.github.com>
2019-10-05 14:32:34 +0530

    Update lotteryContract.sol

--- lotteryContract.sol
+++ lotteryContract.sol
@@ -1,29 +1,36 @@
 pragma solidity ^0.5.6;
 
-contract Lottery {
+contract Lottery 
+   {
     address public manager;
     address payable[] public players;
     event winnerEvent (address winner, uint amount);
-    constructor() public {
-        manager = msg.sender;
+    constructor() public 
+    {
+        manager = msg.sender; // The person who deploys the contract is the manager
     }
-    modifier onlyManager() {
+    modifier onlyManager()
+    {
         require(manager == msg.sender,"Only the manager can call this fn");
         _;
     }
-    function () payable external {
+    function () payable external
+    {
         require(msg.value >= 0.01 ether);
         require(manager != msg.sender);
         players.push(msg.sender);
     }
-    function balance() public view onlyManager returns (uint) {
+    function balance() public view onlyManager returns (uint) 
+    {
         //require(manager == msg.sender,"Only the manager can call balance");
         return address(this).balance;
     }
-    function random() public view returns(uint) {
+    function random() public view returns(uint) 
+    {
         return uint(keccak256(abi.encodePacked(block.timestamp,players.length)));
     }
-    function selectWinner() public onlyManager {
+    function selectWinner() public onlyManager 
+    {
         //require(manager == msg.sender,"Only the manager can call winner");
         uint r = random();
         address payable winner;
