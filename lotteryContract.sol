5e9c390651674fb4f85f3c720af9609a64c3e40e
SAMYUKTHA <38600655+samyukthagopalsamy@users.noreply.github.com>
2019-10-05 14:43:03 +0530

    Update lotteryContract.sol

--- lotteryContract.sol
+++ lotteryContract.sol
@@ -7,7 +7,7 @@ contract Lottery
     event winnerEvent (address winner, uint amount);
     constructor() public 
     {
-        manager = msg.sender; // The person who deploys the contract is the manager
+        manager = msg.sender; // The person who deploys the contract is the manager.
     }
     modifier onlyManager()
     {
@@ -16,11 +16,11 @@ contract Lottery
     }
     function () payable external
     {
-        require(msg.value >= 0.01 ether);
-        require(manager != msg.sender);
+        require(msg.value >= 0.01 ether); // Minimum amount of ether required for lottery is 0.01
+        require(manager != msg.sender); // The manager cannot withdraw the money for himself
         players.push(msg.sender);
     }
-    function balance() public view onlyManager returns (uint) 
+    function balance() public view onlyManager returns (uint) // Only the manager can view the total amount present in the lottery contract
     {
         //require(manager == msg.sender,"Only the manager can call balance");
         return address(this).balance;
@@ -28,15 +28,16 @@ contract Lottery
     function random() public view returns(uint) 
     {
         return uint(keccak256(abi.encodePacked(block.timestamp,players.length)));
+        //returns a very big pseudo-random integer number
     }
-    function selectWinner() public onlyManager 
+    function selectWinner() public onlyManager // winner can be selected only by the manager
     {
         //require(manager == msg.sender,"Only the manager can call winner");
-        uint r = random();
-        address payable winner;
+        uint r = random(); // rondomization functions are not available in smart contract
+        address payable winner; // The total amount present in the contract is transferred to the winner
         uint index = r % players.length;
         winner = players[index];
-        require(manager != winner);
+        require(manager != winner); // The winner should not be manager
         emit winnerEvent(winner,address(this).balance);
         winner.transfer(address(this).balance);
         players = new address payable[](0);
